31d562904865b99f4ac592fbb01e01b10b905184
Matt Tilley <mtilley255@gmail.com>
2018-05-13 09:27:13 -0500

    Page 41 location 1397.

--- C3_LISTS/lists.py
+++ C3_LISTS/lists.py
@@ -1,9 +1,15 @@
-#3-3 YOUR OWN LIST
-vehicles = ["Harley", "Ford", "Honda", "Chevy"]
-models = ["Softail", "Transit Connect", "Civic Hybrid", "Volt"]
-
-message = "I would like to own a " + vehicles[1] + " " + models[1] + " someday."
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+motorcycles[1] = "harley-davidson"
+print(motorcycles)
+message = "I ride " + motorcycles[1].title() + " motorcycles.  I would rather eat worms, than ride a " + motorcycles[0].title() + "."
 print(message)
+###3-3 YOUR OWN LIST
+##vehicles = ["Harley", "Ford", "Honda", "Chevy"]
+##models = ["Softail", "Transit Connect", "Civic Hybrid", "Volt"]
+##
+##message = "I would like to own a " + vehicles[1] + " " + models[1] + " someday."
+##print(message)
 
 
 ##names = ["mike", "jeff", "john", "chris", "brandy", "shan"]
