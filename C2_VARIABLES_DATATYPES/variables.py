6ab371915d38a9726eae037b04a19d7cff6a3ccf
Matt Tilley <mtilley255@gmail.com>
2018-05-12 19:58:30 -0500

    PG 25 location 1077 per example.

--- C2_VARIABLES_DATATYPES/variables.py
+++ C2_VARIABLES_DATATYPES/variables.py
@@ -5,7 +5,13 @@ full_name = first_name + " " + last_name
 
 ##print(full_name.title())
 
-print("Hello, " + full_name.title() + "!")
+##print("Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+
+print(message)
+
+
 
 
 ##name = "ADA LOVELACE"
