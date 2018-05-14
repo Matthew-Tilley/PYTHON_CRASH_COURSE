f71a29c38f9db9730b32991d60277ca38beae696
Matt Tilley <mtilley255@gmail.com>
2018-05-14 10:34:23 -0500

    Page 68 location 2027.

--- C4_MORE_LISTS/more_lists.py
+++ C4_MORE_LISTS/more_lists.py
@@ -1,16 +1,25 @@
 my_foods = ["pizza", "falafel", "carrot cake"]
 
-friend_foods = my_foods[ : ]
-friend_foods.append("ice cream")
-my_foods.append("cannoli")
+friend_foods = my_foods
 
-print("My favorite foods are: ")
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
 print(my_foods)
 
-print("\n")
-
-print("My friend's favorite foods are:")
 print(friend_foods)
+
+##friend_foods = my_foods[ : ]
+##friend_foods.append("ice cream")
+##my_foods.append("cannoli")
+##
+##print("My favorite foods are: ")
+##print(my_foods)
+##
+##print("\n")
+##
+##print("My friend's favorite foods are:")
+##print(friend_foods)
 
 
 
