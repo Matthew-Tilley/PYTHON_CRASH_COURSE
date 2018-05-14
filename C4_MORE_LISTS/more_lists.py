cd562b986bfe48004724126b653242a6d0d702b0
Matt Tilley <mtilley255@gmail.com>
2018-05-13 19:32:04 -0500

    Page 55 location 1720

--- C4_MORE_LISTS/more_lists.py
+++ C4_MORE_LISTS/more_lists.py
@@ -1,11 +1,13 @@
 magicians = ["alice", "david", "carolina"]
 
 for magician in magicians:
-    print(magician)
-    
-cats = ["jade", "barack", "cali", "otis", "kizzy"]
-
-for cat in cats:
-    print(cat)
+    print(magician.title() + ", that was a great trick!")
+  
+  
+  
+##cats = ["jade", "barack", "cali", "otis", "kizzy"]
+##
+##for cat in cats:
+##    print(cat)
     
     
