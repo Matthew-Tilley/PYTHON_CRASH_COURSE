fb529c4cf31703922207d77f77f7b667c07434b0
Matt Tilley <mtilley255@gmail.com>
2018-05-17 11:18:38 -0500

    Tinkering around a little / warming up.

---
+++ C5_IF_STATEMENTS/if_statements.py
@@ -0,0 +1,14 @@
+a = "3"
+
+b = 3
+
+print(a = b)
+
+##cars = ['audi', 'bmw', 'subaru', 'toyota']
+##
+##for car in cars:
+##        if car == "bmw":
+##            print(car.upper())
+##        
+##        else:
+##            print(car.title())
