611b38e9f97a4f682ab61e4ea4eacdd9bb6f1c5d
Matt Tilley <mtilley255@gmail.com>
2018-05-18 11:04:38 -0500

    Page 96 location 2704.

--- C5_IF_STATEMENTS/if_statements.py
+++ C5_IF_STATEMENTS/if_statements.py
@@ -1,25 +1,32 @@
-#5-11 ORDINAL NUMBERS
+alien_0 = {
+    "color": "green",
+    "points": 5
+}
 
-numz = [1,2,3,4,5,6,7,8,9]
+print(alien_0['color'])
 
-for num in numz:
-    
-    if num == 1:
-        print(str(num) + "st")
-    
-    if num == 2:
-        print(str(num) + "nd")
-    
-    if num == 3:
-        print(str(num) + "rd")
         
-##    if num == 4 or num == 5 or num == 6 or num == 7 or num == 8 or num == 9:
+
+
+###5-11 ORDINAL NUMBERS
+##
+##numz = [1,2,3,4,5,6,7,8,9]
+##
+##for num in numz:
+##    
+##    if num == 1:
+##        print(str(num) + "st")
+##    
+##    if num == 2:
+##        print(str(num) + "nd")
+##    
+##    if num == 3:
+##        print(str(num) + "rd")
+##        
+####    if num == 4 or num == 5 or num == 6 or num == 7 or num == 8 or num == 9:
+####        print(str(num) + "th")
+##    if num >= 4 and num < 10:
 ##        print(str(num) + "th")
-    if num >= 4 and num < 10:
-        print(str(num) + "th")
-        
-    
-
 
 
 
