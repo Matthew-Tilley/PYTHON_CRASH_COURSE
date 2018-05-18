b1d6eff5c3afd82f809abce91f357b18474921c9
Matt Tilley <mtilley255@gmail.com>
2018-05-18 08:38:07 -0500

    Page 91 location 2598.

--- C5_IF_STATEMENTS/if_statements.py
+++ C5_IF_STATEMENTS/if_statements.py
@@ -1,13 +1,26 @@
 
-requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
+#requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
+requested_toppings = []
 
-for requested_topping in requested_toppings:
-    
-    if requested_topping == "green peppers":
-        print("Sorry, we are out of green peppers right now.")
-    
-    else:
+if requested_toppings:
+    for requested_topping in requested_toppings:
         print("Adding " + requested_topping + ".")
+else:
+    print("Are you sure you want a plain pizza?")
+
+
+##for requested_topping in requested_toppings:
+##    
+##    if requested_toppings == undefined:
+##        print("Are you sure you want a plain pizza?")
+##    
+##    if requested_topping == "green peppers":
+##        print("Sorry, we are out of green peppers right now.")
+##    
+##    #else:
+##        #print("Adding " + requested_topping + ".")
+##    
+    
 
 print("\nFinished making your pizza!")
 
