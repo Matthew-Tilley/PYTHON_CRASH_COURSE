784890e1900ea2a0c234bf1d18db260a483f9872
Matt Tilley <mtilley255@gmail.com>
2018-05-18 11:36:13 -0500

    Page 99 location 2779.

--- C5_IF_STATEMENTS/if_statements.py
+++ C5_IF_STATEMENTS/if_statements.py
@@ -3,18 +3,47 @@ alien_0 = {
     "points": 5
 }
 
+alien_0 = {
+    "x_position": 0,
+    "y_position": 25,
+    "speed": "medium"
+    
+    }
+
+print("Original X position was: " + str(alien_0["x_position"]))
+
+if alien_0["speed"] == "slow":
+    x_increment = 1
+    
+elif alien_0["speed"] == "medium":
+    x_increment = 2
+
+else:
+    x_increment = 3
+    
+alien_0["x_position"] = alien_0["x_position"] + x_increment
+
+print("New x-position: " + str(alien_0["x_position"]))
+
+
+
 #print(alien_0['color'])
 
-print(alien_0["color"])
+##print(alien_0["color"])
+##
+##new_points = alien_0["points"]
+##
+##print("You just earned " + str(new_points) + " points!")
+##
+##
+##alien_0['x_position'] = 0
+##alien_0['y_position'] = 25
+##print(alien_0)
 
-new_points = alien_0["points"]
+##alien_0["color"] = "yellow"
+##print("The alien is now: " + alien_0["color"])
 
-print("You just earned " + str(new_points) + " points!")
 
-
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
-print(alien_0)
 
         
 
