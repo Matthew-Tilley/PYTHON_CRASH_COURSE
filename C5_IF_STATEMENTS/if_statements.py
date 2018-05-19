dd96720f013518f49fd9aee6e35d8cec4bb65b2c
Matt Tilley <mtilley255@gmail.com>
2018-05-19 12:34:59 -0500

    Start of session save.

--- C5_IF_STATEMENTS/if_statements.py
+++ C5_IF_STATEMENTS/if_statements.py
@@ -15,12 +15,16 @@ print("Original X position was: " + str(alien_0["x_position"]))
 if alien_0["speed"] == "slow":
     x_increment = 1
     
-elif alien_0["speed"] == "medium":
+if alien_0["speed"] == "medium":
     x_increment = 2
 
 else:
     x_increment = 3
     
+    
+alien_0["speed"] = "slow"
+
+
 alien_0["x_position"] = alien_0["x_position"] + x_increment
 
 print("New x-position: " + str(alien_0["x_position"]))
